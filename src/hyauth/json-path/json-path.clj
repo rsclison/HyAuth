619dfdc8af82542ba617a5c03311d26a6e85b5b1
rsclison <raymond.sclison@asso-cocktail.fr>
2022-07-19 17:19:34 +0200

    xx

---
+++ src/hyauth/json-path/json-path.clj
@@ -0,0 +1,10 @@
+(ns hyauth.json-path.json-path
+  [:require [json-path.parser :as parser]
+   [json-path.match :as m]
+   [json-path.walker :as walker]])
+
+(defn query [path object]
+  (walker/walk (parser/parse-path path) {:root (m/root object)}))
+
+(defn at-path [path object]
+  (walker/map# :value (query path object)))
---
+++ src/hyauth/json-path/match.clj
@@ -0,0 +1,13 @@
+(ns hyauth.json-path.match)
+
+(defrecord Match [path value])
+
+(defn root [value]
+  (->Match [] value))
+
+(defn with-context
+  ([key value context] (->Match (vec (concat (:path context) [key]))
+                                value)))
+
+(defn create [value path]
+  (->Match path value))
